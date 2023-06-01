37207ffc17f6fd3a472cca2a5103cef55ab66e0a
mudit3113 <116980345+mudit3113@users.noreply.github.com>
2023-06-01 23:51:01 +0530

    creating ImageGrid component

--- src/Components/CustomerCard/index.js
+++ src/Components/CustomerCard/index.js
@@ -1,30 +1,14 @@
-import react from "react";
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import "./_customer-card.scss";
-import React, { useEffect } from "react";
-import { fetchRandomImages } from "../../Redux/Actions/customerAction";
+import React from "react";
+import { ImageGrid } from '../ImageGrid';
 
 export const CustomerCard = () => {
-    const dispatch = useDispatch();
     const customers = useSelector((state) => state.customers)
     // console.log("customers",customers);
     const selectedCustomerId = useSelector((state) => state.selectedCustomer)
     const customer = customers.filter((item) => item.id == selectedCustomerId)
-    const images = useSelector((state) => state.images);
-    console.log("images", images);
     // console.log("customer",customer);
-    // using useEffect hook to call sideEffect - fetchAPI 
-
-    useEffect(() => {
-        const fetchImagesInterval = () => {
-            dispatch(fetchRandomImages());
-            setTimeout(fetchImagesInterval,10000);
-        }
-        fetchImagesInterval();
-        return()=> {
-            clearTimeout()
-        }
-    }, [])
 
     return (
         <>
@@ -35,21 +19,12 @@ export const CustomerCard = () => {
                             <p>Name: {item.name}</p>
                             <p>Title: {item.title}</p>
                             <p>  Address:{item.address}</p>
-                            <hr />
                         </div>
                     ))}
 
                 </div>
-                <div className="image-container">
-                    <div className="image-grid">
-                        {images.map((image) => (
-                            <div className="image-cell">
-                                <img  className="image-cell-avatar" src={image.url} alt={image.title}/>
-                            </div>
-                        ))}
-                    </div>
-                </div>
-
+                <ImageGrid />
+                
             </div>
 
 
