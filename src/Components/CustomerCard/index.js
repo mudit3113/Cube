d7cf4182130dab70c3ab5c45f7e469fac6d35d34
mudit3113 <116980345+mudit3113@users.noreply.github.com>
2023-06-02 00:00:21 +0530

    resolving conflict bringing master file

--- src/Components/CustomerCard/index.js
+++ src/Components/CustomerCard/index.js
@@ -1,14 +1,23 @@
-import { useSelector } from 'react-redux';
+import react from "react";
+import { useSelector, useDispatch } from 'react-redux';
 import "./_customer-card.scss";
-import React from "react";
-import { ImageGrid } from '../ImageGrid';
+import React, { useEffect } from "react";
+import { fetchRandomImages } from "../../Redux/Actions/customerAction";
 
 export const CustomerCard = () => {
+    const dispatch = useDispatch();
     const customers = useSelector((state) => state.customers)
     // console.log("customers",customers);
     const selectedCustomerId = useSelector((state) => state.selectedCustomer)
     const customer = customers.filter((item) => item.id == selectedCustomerId)
+    const images = useSelector((state) => state.images);
+    console.log("images", images);
     // console.log("customer",customer);
+  
+    // using useEffect hook to call sideEffect - fetchAPI 
+        useEffect(() => {
+            dispatch(fetchRandomImages());
+        },[])
 
     return (
         <>
@@ -19,12 +28,21 @@ export const CustomerCard = () => {
                             <p>Name: {item.name}</p>
                             <p>Title: {item.title}</p>
                             <p>  Address:{item.address}</p>
+
                         </div>
                     ))}
 
                 </div>
-                <ImageGrid />
-                
+                <div className="image-container">
+                    <div className="image-grid">
+                        {images.map((image) => (
+                            <div className="image-cell">
+                                <img className="image-cell-avatar" src={image.url} alt={image.title} />
+                            </div>
+                        ))}
+                    </div>
+                </div>
+
             </div>
 
 
